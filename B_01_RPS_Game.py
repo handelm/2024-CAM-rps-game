8490a9c01eadd953692dec357f7bd288f46a825c
handelm <161265311+handelm@users.noreply.github.com>
2024-03-20 09:43:35 +1300

    Add files via upload

---
+++ B_01_RPS_Game.py
@@ -0,0 +1,8 @@
+# Create a game of Rock Paper Scissors
+
+print(" Welcome to the game of Rock Paper Scissors")
+# ask the user how many rounds he/she wants to play
+
+# ask if they want to see the instructions
+
+# 
